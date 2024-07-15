7a45a574b7933314ae67cfe59727046499932622
W. Garrett Mitchener <garrett.mitchener@gmail.com>
2024-07-15 17:28:48 -0400

    Adding fuzzy logic and thresholding

--- src/Operations.jl
+++ src/Operations.jl
@@ -1,6 +1,8 @@
 export splat_or_default
 export Add, Multiply, ReciprocalMultiply
 export Subtract
+export FuzzyAnd, FuzzyOr, FuzzyNand
+export SignAdd
 
 """
     splat_or_default(op, def, operands)
@@ -95,7 +97,7 @@ end
 struct ReciprocalMultiply <: AbstractGeneOp end
 short_show(io::IO, ::ReciprocalMultiply) = print(io, "rcpm")
 op_eval(::ReciprocalMultiply, operands) = 1 ./ splat_or_default(.*, 1, operands)
-function to_expr(::ReciprocalMultiply, w, operands)
+function to_expr(::ReciprocalMultiply, cs, operands)
     if isempty(operands)
         return 1
     elseif length(operands) == 1
@@ -105,4 +107,78 @@ function to_expr(::ReciprocalMultiply, w, operands)
         return Expr(:call, :./, 1,
             Expr(:call, :.*, (:($cs.$field[$j]) for (field, j) in operands)...))
     end
+end
+
+"Return fuzzy AND of the operands"
+struct FzAnd <: AbstractGeneOp end
+short_show(io::IO, ::FzAnd) = print(io, "fzAnd")
+op_eval(::FzAnd, operands) = splat_or_default(.*, 1, operands)
+function to_expr(::FzAnd, cs, operands)
+    if isempty(operands)
+        return 1
+    elseif length(operands) == 1
+        field, j = operands[1]
+        return :($cs.$field[$j])
+    else
+        return Expr(:call, :.*, (:($cs.$field[$j]) for (field, j) in operands)...)
+    end
+end
+
+"Return fuzzy OR of the operands."
+struct FzOr <: AbstractGeneOp end
+short_show(io::IO, ::FzOr) = "fzOr"
+function op_eval(::FzOr, operands)
+    return 1 .- splat_or_default(.*, 1, 1 .- operands)
+end 
+function to_expr(::FzOr, cs, operands)
+    if isempty(operands)
+        return 0
+    elseif length(operands) == 1
+        field, j = operands[1]
+        return :($cs.$field[$j])
+    else
+        return Expr(:call, :.-, 1,
+            Expr(:call, :.*, (:(1 .- $cs.$field[$j]) for (field, j) in operands)...))        
+    end
+end
+
+
+"Return fuzzy NAND of the operands."
+struct FzNand <: AbstractGeneOp end
+short_show(io::IO, ::FzNand) = "fzNand"
+function op_eval(::FzNand, operands)
+    return 1 .- splat_or_default(.*, 1, operands)
+end
+function to_expr(::FzNand, cs, operands)
+    if isempty(operands)
+        return 0
+    elseif length(operands) == 1
+        field, j = operands[1]
+        return :(1 .- $cs.$field[$j])
+    else
+        return Expr(:call, :.-, 1,
+            Expr(:call, :.*, (:($cs.$field[$j]) for (field, j) in operands)...))        
+    end
+end
+
+"Return the sign of the sum of the operands"
+struct SignAdd <: AbstractGeneOp end
+short_show(io::IO, ::SignAdd) = "signAdd"
+function op_eval(::SignAdd, operands)
+    return sign.(splat_or_default(.+, 0, operands))
+end
+function to_expr(::SignAdd)
+    function to_expr(::Add, cs, operands)
+        if isempty(operands)
+            return 0
+        elseif length(operands) == 1
+            field, j = operands[1]
+            return :(sign.($cs.$field[$j]))
+        else
+            return quote
+                sign.((.+)($($cs.$field[$j]) for (field, j) in operands)...)
+            end
+        end
+    end
+    
 end
