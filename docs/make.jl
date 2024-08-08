44a7c0d08e1e1848998693c92f5d06d746616bec
W. Garrett Mitchener <garrett.mitchener@gmail.com>
2024-08-08 17:19:19 -0400

    Setting up a Documenter.jl workflow file for GitHub

--- docs/make.jl
+++ docs/make.jl
@@ -9,6 +9,6 @@ makedocs(
 # Documenter can also automatically deploy documentation to gh-pages.
 # See "Hosting Documentation" and deploydocs() in the Documenter manual
 # for more information.
-# deploydocs(
-#     repo = "github.com/wgm-applied-math/Jessamine.jl/"
-# )
+deploydocs(
+    repo = "github.com/wgm-applied-math/Jessamine.jl.git"
+)
