1c3059953c52a0f80ac1fcbacf6eafa8498d10ee
W. Garrett Mitchener <garrett.mitchener@gmail.com>
2025-01-16 17:22:23 -0500

    Checked result of run_genome_time_series

--- test/TestTimeSeries.jl
+++ test/TestTimeSeries.jl
@@ -17,6 +17,14 @@ function main()
 
     println("Test run_genome_time_series")
 
+    a = 1.4
+    ma = -a
+    b = 0.3
+
+    f((x, y), (xm, ym)) = [
+        1 - a * x^2 + y + xm,
+        b * x + ym]
+
     g_spec = GenomeSpec(2, 1, 3, 4, 2)
 
     input_mat = Float64[
@@ -26,19 +34,23 @@ function main()
     ]
 
     input_series = eachrow(input_mat)
-
     @show input_series
 
-    a = 1.4
-    ma = -a
-    b = 0.3
+    expected_output_series = Vector{Float64}[]
+    u = [0, 0]
+    for r in input_series
+        u = f(r, u)
+        push!(expected_output_series, u)
+    end
+
+    @show expected_output_series
 
     # Map from variables to indices
     x1, y1, t1, p1, pma, pb, x0, y0, xm1, ym1 = 1:10
 
     instruction_blocks = [
         [Instruction(Add(), [p1, t1, y0, xm1])],
-        [Instruction(Multiply(), [pb, x0, ym1])],
+        [Instruction(Multiply(), [pb, x0]), Instruction(Add(), [ym1])],
         [Instruction(Multiply(), [pma, x0, x0])]
     ]
 
